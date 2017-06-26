elfring/Coccinelle-20160205

commit 9597598779f3604272621f6d42ead79eaafad2a8

Bug #104: Improve some dependency specifications in make rules
A bit of progress was achieved with one experiment for build script adjustments.
Further changes can be tried out. Unfortunately, an error message like the
following from the program “GNU Make 4.2.1-1.7” was noticed then.
https://systeme.lip6.fr/pipermail/cocci/2017-June/004181.html

…
/usr/bin/ocamlopt.opt …
File "cocci.ml", line 1:
Error: Could not find the .cmi file for interface cocci.mli.
make[3]: *** [Makefile:533: cocci.cmx] Error 2
…


Suffix rules were used to generate the desired software components usually.
These rules were correctly used in the documented sense that no extra
prerequisites were specified there. This means also that usual dependencies
on other software modules were not directly expressed by default.
Extra dependencies were integrated by inclusion of the make script “.depend”
which was automatically generated by the tool “ocamldep”.

Further tests pointed places out despite of this detail where the build approach
failed in a “special” software development configuration.


The affected make rules were also repeated in several scripts. They were moved
to a separate script and replaced by pattern rules with the addition of an
explicit dependency on interface descriptions in the form of mli files for
the programming language “OCaml”. This change increased the relevance for
these files. But it had also the consequence that tolerance had to be
explicitly expressed for software areas which work also without such
interface descriptions. The make rules were extended as needed then.


Further build tests were performed which showed unexpected results.

Open issue 1)
elfring@Sonne:~/Projekte/Coccinelle/20160205/globals> make clean && LANG=C make --no-builtin-rules V=1 && make clean && LANG=C make --no-builtin-rules -j4 V=1
rm -f …
…

common_rules.make
@@ -1,0 +1,15 @@
+# This file is part of Coccinelle, licensed under the terms of the GPL v2.
+# See copyright.txt in the Coccinelle source code for more information.
+# The Coccinelle source code can be obtained at http://coccinelle.lip6.fr
+
+%.cmi: %.mli
+	$(OCAMLC_CMD) -c $<
+
+%.cmo: %.ml %.cmi
+	$(OCAMLC_CMD) -c $<
+
+%.cmx: %.ml %.cmi
+	$(OCAMLOPT_CMD) -c $<
+
+%.mldepend: %.ml
+	$(OCAMLC_CMD) -i $<

Makefile
@@ -40,7 +40,7 @@ TARGET=spatch
 PRJNAME=coccinelle
 ML_FILES=flag_cocci.ml cocci.ml testing.ml $(LEXER_SOURCES:.mll=.ml) \
 read_options.ml main.ml
-MLI_FILES=cocci.mli testing.mli
+MLI_FILES=cocci.mli read_options.mli testing.mli
 
 ifeq ($(FEATURE_PYTHON),1)
 	PYTHON_INSTALL_TARGET=install-python
@@ -515,15 +515,15 @@ tags:
 
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
-	$(OCAMLC_CMD) -c $<
-.mli.cmi:
-	$(OCAMLC_CMD) -c $<
-.ml.cmx:
-	$(OCAMLOPT_CMD) -c $<
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/common_rules.make
 
-.ml.mldepend:
-	$(OCAMLC_CMD) -i $<
+flag_cocci.cmo main.cmo: %.cmo: %.ml
+	$(OCAMLC_CMD) -c $<
+
+flag_cocci.cmx main.cmx: %.cmx: %.ml
+	$(OCAMLOPT_CMD) -c $<
+endif
 
 clean distclean::
 	rm -f .depend

Makefile.config.in
@@ -51,6 +51,7 @@ includedir=@includedir@
 libdir=@libdir@
 sysconfdir=@sysconfdir@
 mandir=@mandir@
+abs_top_srcdir::=@abs_top_srcdir@
 
 # C compiler
 export CC=@CC@

commons/Makefile
@@ -208,12 +208,15 @@ dependencygraph2:
 .SUFFIXES:
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
-	$(OCAMLC_CMD)  -c $<
-.mli.cmi:
-	$(OCAMLC_CMD)  -c $<
-.ml.cmx:
-	$(OCAMLOPT_CMD)  -c $<
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/common_rules.make
+
+commands.cmo: commands.ml
+	$(OCAMLC_CMD) -c $<
+
+commands.cmx: commands.ml
+	$(OCAMLOPT_CMD) -c $<
+endif
 
 clean::
 	rm -f *.cm[iox] *.o *.a *.cma *.cmxa *.annot

ctl/Makefile
@@ -9,7 +9,8 @@ endif
 
 TARGET=ctl
 
-SRC=flag_ctl.ml ast_ctl.ml pretty_print_ctl.ml ctl_engine.ml wrapper_ctl.ml
+SRC_without_mli::=ast_ctl.ml flag_ctl.ml
+SRC_with_mli::=ctl_engine.ml pretty_print_ctl.ml wrapper_ctl.ml
 
 SYSLIBS=str.cma unix.cma
 LIBS=../commons/commons.cma ../globals/globals.cma
@@ -30,8 +31,8 @@ OCAMLMKTOP_CMD=$(OCAMLMKTOP) -g -custom $(INCLUDES)
 LIB=$(TARGET).cma
 OPTLIB=$(LIB:.cma=.cmxa)
 
-OBJS = $(SRC:.ml=.cmo)
-OPTOBJS = $(SRC:.ml=.cmx)
+OBJS::=$(SRC_without_mli:.ml=.cmo) $(SRC_with_mli:.ml=.cmo)
+OPTOBJS::=$(SRC_without_mli:.ml=.cmx) $(SRC_with_mli:.ml=.cmx)
 
 
 all: $(LIB)
@@ -54,15 +55,20 @@ clean::
 .SUFFIXES:
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/common_rules.make
+include $(abs_top_srcdir)/rule_pair.make
+
+pretty_print_ctl.cmo: pretty_print_ctl.ml pretty_print_ctl.cmi \
+ast_ctl.cmi \
+../commons/common.cmi
 	$(OCAMLC_CMD) -c $<
 
-.mli.cmi:
-	$(OCAMLC_CMD) -c $<
-
-.ml.cmx:
+pretty_print_ctl.cmx: pretty_print_ctl.ml pretty_print_ctl.cmi \
+ast_ctl.cmi \
+../commons/common.cmi
 	$(OCAMLOPT_CMD) -c $<
-
+endif
 
 # clean rule for other files
 clean::

engine/Makefile
@@ -84,15 +84,9 @@ clean::
 
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
-	$(OCAMLC_CMD) -c $<
-.mli.cmi:
-	$(OCAMLC_CMD) -c $<
-.ml.cmx:
-	$(OCAMLOPT_CMD) -c $<
-
-.ml.mldepend:
-	$(OCAMLC_CMD) -i $<
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/modules_without_mli.make
+endif
 
 clean::
 	rm -f *.cm[ioxa] *.o *.a *.cmxa *.annot

extra/Makefile
@@ -51,15 +51,9 @@ clean::
 
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
-	$(OCAMLC_CMD) -c $<
-.mli.cmi:
-	$(OCAMLC_CMD) -c $<
-.ml.cmx:
-	$(OCAMLOPT_CMD) -c $<
-
-.ml.mldepend:
-	$(OCAMLC_CMD) -i $<
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/common_rules.make
+endif
 
 clean::
 	rm -f .depend

globals/Makefile
@@ -15,7 +15,8 @@ TARGET=globals
 OCAMLCFLAGS ?= -g
 OPTFLAGS ?= -g
 
-SRC=config.ml flag.ml iteration.ml $(REGEXP_FILE) regexp.ml
+SRC_without_mli::=config.ml flag.ml $(REGEXP_FILE) regexp.ml
+SRC_with_mli::=iteration.ml
 
 LIBS=
 INCLUDEDIRS= ../commons $(PCREDIR)
@@ -30,8 +31,8 @@ OCAMLOPT_CMD=$(OCAMLOPT) $(OPTFLAGS) $(INCLUDES)
 OCAMLDEP_CMD=$(OCAMLDEP) $(INCLUDES)
 OCAMLMKTOP_CMD=$(OCAMLMKTOP) -g -custom $(INCLUDES)
 
-OBJS= $(SRC:.ml=.cmo)
-OPTOBJS= $(SRC:.ml=.cmx)
+OBJS::=$(SRC_without_mli:.ml=.cmo) $(SRC_with_mli:.ml=.cmo)
+OPTOBJS::=$(SRC_without_mli:.ml=.cmx) $(SRC_with_mli:.ml=.cmx)
 
 ##############################################################################
 # Top rules
@@ -52,15 +53,16 @@ $(TARGET).cmxa: $(LIBS:.cma=.cmxa) $(OPTOBJS)
 ##############################################################################
 .SUFFIXES: .ml .mli .cmo .cmi .cmx
 
-.ml.cmo:
-	$(OCAMLC_CMD) -c $<
-.mli.cmi:
-	$(OCAMLC_CMD) -c $<
-.ml.cmx:
-	$(OCAMLOPT_CMD) -c $<
+ifneq ($(strip $(abs_top_srcdir)),)
+include $(abs_top_srcdir)/common_rules.make
+include $(abs_top_srcdir)/rule_pair.make
 
-.ml.mldepend:
-	$(OCAMLC_CMD) -i $<
+iteration.cmo: iteration.ml iteration.cmi flag.cmi
+	$(OCAMLC_CMD) -c $<
+
+iteration.cmx: iteration.ml iteration.cmi flag.cmi
+	$(OCAMLOPT_CMD) -c $<
+endif
 
 clean:
 	rm -f *.cm[ioxa] *.o *.a *.cmxa *.annot

commit 13fd24aff54fd86ded5a3093f927f0fed53d1f99

Correction of dependency specifications for two common make rules
It was noticed that make pattern rules did not work as it was “hoped”
when two “patterns” were specified in the prerequisites list for
OCaml source files and their corresponding compiled interface descriptions.

This issue can be clarified further by the bug report
“Checking search retries for implicit make rules” (from 2017-06-25).
https://savannah.gnu.org/bugs/?51311

It seems that a replacement with static pattern rules was more appropriate
in this use case.
https://www.gnu.org/software/make/manual/html_node/Static-Usage.html

But they have got an other software behaviour with the consequence
that messages like the following can occur then usually.

“…
…/rule_pair.make:13: warning: overriding recipe for target 'ast_ctl.cmo'
…/common_rules.make:11: warning: ignoring old recipe for target 'ast_ctl.cmo'
…”


Fixes: 9597598779f3604272621f6d42ead79eaafad2a8 ("Bug 104: Improve some dependency specifications in make rules")

Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

common_rules.make
@@ -5,10 +5,10 @@
 %.cmi: %.mli
 	$(OCAMLC_CMD) -c $<
 
-%.cmo: %.ml %.cmi
+$(OBJS): %.cmo: %.ml %.cmi
 	$(OCAMLC_CMD) -c $<
 
-%.cmx: %.ml %.cmi
+$(OPTOBJS): %.cmx: %.ml %.cmi
 	$(OCAMLOPT_CMD) -c $<
 
 %.mldepend: %.ml